62aab56099276c2eff58e83cc7d3c8726ddb1a6c
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-22 20:07:10 -0500

    Correct error.

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const app = require("./app.js");
 
-require("dotenv").config();
-const PORT = process.env.PORT || 8888;
-app.listen(PORT, () => {
-  console.log(`iSUPPORT live on port: ${PORT}`);
-});
