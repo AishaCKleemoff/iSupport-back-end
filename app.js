bbd594955a1d72b30af7f5a09931a5ce9d9235d7
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-25 08:31:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = requir("express");
 const cors = require("cors");
 const app = express();
+const support_groupsController = require("./controllers/support_groupsController");
 
 app.use(cors());
 app.use(express.json());
+app.use("/support_groups", support_groupsController);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Aisha's support_group app.");
+});
+
+app.get("*", (req, res) => {
+  res
+    .status(404)
+    .json({ success: false, data: { error: "Sorry page could not be found" } });
+});
+module.exports = app;
